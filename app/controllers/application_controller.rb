b41dbe4ac79d5c15d082ddd28e2cab044b5327ac
PabiGamito <pablogamito@gmail.com>
2015-07-21 11:18:57 -0400

    Fwitter 1.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,24 +4,29 @@ class ApplicationController < Sinatra::Base
   
 	configure do
 		set :views, "app/views"
-		set :public, "public"
+		set :public_dir, "public"
 		set :models, "/app/models"
 	end
 	
   get "/" do
-		#make 3 new tweets
-		#save all tweets in one instance variable as an array
-		#pass that instance variable into the view useing ERB
-		@tweet1=Tweet.new("Chloe","Hello owner of this computer!")
-		@tweet2=Tweet.new("Donald Trump","I can't control my hair, but I can control a country! #Trump2016")
-		@tweet3=Tweet.new("Chloe","Sorry if you are a republican...")
-		@tweets=[]
-		
-		Tweet.all.each do |tweet_object|
-			@tweets.push("#{tweet_object.username} says: #{tweet_object.tweet}")
-		end
-		
 		erb :tweets
   end
+
+  get '/results' do
+  	erb :results
+	end
+
+  post '/' do
+  	@tweet= Tweet.new(params[:username], params[:fweet])
+  	@time= Time.new
+  	@tweets = []
+
+  	Tweet.all.each do |tweet_object|
+			@tweets.push("#{tweet_object.username} #{tweet_object.tweet}")
+		end
+
+  	erb :results
+  end
+
   
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,3 @@
-@array=[]
 class Tweet
 	ALL_TWEETS = []
 	attr_accessor :username, :tweet, :all_tweets
@@ -13,9 +12,6 @@ class Tweet
 	# Should initialize with content and a username
 	# The tweet class keep track of all instances of tweets that get created
 end
-
-tweet1=Tweet.new("Ian", "I am too blessed to be stressing")
-tweet2=Tweet.new("Donald", "Time to make the right move #ChangeAmerica")
 
 @all_tweets = Tweet.all
 puts Tweet.all
